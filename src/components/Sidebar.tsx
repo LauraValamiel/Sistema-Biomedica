1130e0357786d4e889f06d5f0020918b25f096ac
Laura Valamiel <valamiellaura4@gmail.com>
2026-09-21 14:00:54 -0300

    Correção do ícone de logout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -136,7 +136,7 @@ export default function Sidebar({ onCloseMobile }: SidebarProps) {
         >
           <span className="shrink-0">
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.7} d="M17 16l4-4m0 0l-4-4m4 4H7v6m4-6v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.7} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
             </svg>
           </span>
           <span>Sair da Conta</span>
